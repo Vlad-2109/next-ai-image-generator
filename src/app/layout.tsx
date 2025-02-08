62fa5ec36665e360e626c0762bf3d97e733cd62e
Vlad-2109 <vladswim21@gmail.com>
2025-02-08 20:26:00 +0200

    add footer section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
-import "./globals.css";
-import ResponsiveNavbar from "@/components/Home/Navbar/ResponsiveNavbar";
-import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import ResponsiveNavbar from '@/components/Home/Navbar/ResponsiveNavbar';
+import { Toaster } from '@/components/ui/sonner';
+import Footer from '@/components/Home/Footer/Footer';
+import './globals.css';
 
 const font = Roboto({
 	weight: ['100', '300', '400', '500', '700', '900'],
@@ -17,13 +18,16 @@ export const metadata: Metadata = {
 	},
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
+export default function RootLayout({
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
 		<html lang="en">
-		  <body className={`${font.className} bg-gray-900 antialiased`}>
-			  <ResponsiveNavbar />
-			  {children}
-			  <Toaster />
+			<body className={`${font.className} bg-gray-900 antialiased`}>
+				<ResponsiveNavbar />
+				{children}
+				<Footer />
+				<Toaster />
 			</body>
 		</html>
 	);
